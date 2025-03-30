38cff61128d4bfc1bd98204a7e9924d350f42d24
Hazel <Flo.Lazik@t-online.de>
2025-03-31 00:57:37 +0200

    Beautify output, center it and clean up code

--- app/src/main/java/com/example/learningandroid/MainActivity.kt
+++ app/src/main/java/com/example/learningandroid/MainActivity.kt
@@ -6,17 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.learningandroid.ui.theme.LearningAndroidTheme
+import androidx.compose.ui.unit.sp
+import com.example.learningandroid.ui.theme.GreetingsTheme
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -27,13 +31,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LearningAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Hazel",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            GreetingsTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        GreetingText(
+                            name = "Hazel",
+                            //modifier = Modifier.padding(innerPadding)
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+//                }
             }
         }
     }
@@ -41,7 +51,33 @@ class MainActivity : ComponentActivity() {
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingText(name: String, modifier: Modifier = Modifier) {
+    val greeting = determineDaytime()
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = "Good $greeting, $name!",
+            fontSize = 90.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .padding(20.dp)
+                .padding(bottom = 50.dp)
+        )
+        Text(
+            text = "It's a beautiful day ♥",
+            fontSize = 20.sp,
+            modifier = modifier
+                .align(alignment = Alignment.Start)
+        )
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun determineDaytime() : String {
     val currentHour =
         Integer.parseInt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH")))
     val greeting = if (currentHour in 4..10) {
@@ -54,19 +90,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         "evening"
     }
 
-    Surface(color = Color.Green) {
-        Text(
-            text = "Good $greeting, $name! ♥",
-            modifier = modifier.padding(20.dp)
-        )
-    }
+    return greeting
 }
 
+//Preview Only
 @RequiresApi(Build.VERSION_CODES.O)
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "GreetingsPreviewScreen")
 @Composable
-fun GreetingPreview() {
-    LearningAndroidTheme {
-        Greeting("Hazel")
+fun GreetingCardPreview() {
+    GreetingsTheme {
+        GreetingText(name = "Hazel")
     }
 }
